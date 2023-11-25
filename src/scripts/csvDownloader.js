e5c4a183c561af83498a48da672126e835c3ec7b
Sereneum <54134545+Sereneum@users.noreply.github.com>
2023-11-25 10:11:31 +0000

    csv

--- src/scripts/csvDownloader.js
+++ src/scripts/csvDownloader.js
@@ -1,6 +1,28 @@
 import Papa from 'papaparse';
 
-export const csvDownloader = (data) => {
+const classToNum = (t) => {
+    const r = '0';
+    switch (t) {
+        case 'бетон':
+            r = '1';
+          break;
+        case 'грунт':
+            r = '3';
+          break;
+        case 'дерево':
+            r = '2';
+          break;
+        case 'кирпич':
+            r = '4';
+          break;
+      }
+
+      return r;
+}
+
+
+
+ export const csvDownloader = (data) => {
     console.log({csvDownloader: data});
     const csvData = Papa.unparse(data);
     console.log({csvData});
@@ -21,13 +43,36 @@ export const csvClassesDownloader = (response) => {
         'file_name', 
         'class_name'
     ]];
-    
-    response.forEach((item) => {
+
+    response.forEach((item, index) => {
+        const r = '3';
+            switch (item.class_name) {
+                case 'бетон':
+                    r = '1';
+                break;
+                case 'грунт':
+                    r = '3';
+                break;
+                case 'дерево':
+                    r = '2';
+                break;
+                case 'кирпич':
+                    r = '4';
+                break;
+            }
+
         data.push([
             item.filename ? item.filename : 'no_name', 
-            item.class_name !== 'не определен' ? item.class_name : 'undetected'
+            r
         ])
     })
+    
+    // response.forEach((item) => {
+    //     data.push([
+    //         item.filename ? item.filename : 'no_name', 
+    //         item.class_name !== 'не определен' ? item.class_name : 'undetected'
+    //     ])
+    // })
 
     csvDownloader(data);
 }
